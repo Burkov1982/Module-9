ea647185bb87e533e7712273dc7a8f5f5380cc27
Burkov1982 <burkov_daniil@i.ua>
2021-03-18 14:53:51 +0300

    Homework module 9

--- src/main/java/Task_3/Main.java
+++ src/main/java/Task_3/Main.java
@@ -1,36 +1,68 @@
 package Task_3;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         try(FileReader reader = new FileReader("C:\\Users\\comp\\Desktop\\Module-9\\src\\main\\java\\Task_3\\words.txt")){
-            int c;
-            String inputData = "";
-            while ((c = reader.read()) > 0){
-                inputData += String.valueOf((char) c);
-            }
-            HashMap<String, Integer> hashMap = new HashMap<>();
-            String[] inputDataArr = inputData.split("\\n");
-            ArrayList<String> words = new ArrayList<>();
-            for (String str:inputDataArr) {
-                String[] temp = str.split(" ");
-                for (String str1:temp) {
-                    int size = words.size();
-                    words.add(size, str1);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            ArrayList<String> strings = new ArrayList<>();
+            String temp = bufferedReader.readLine();
+            int counter = 0;
+
+            while (temp!=null){
+                String[] tempArrOfWords = temp.split(" ");
+                temp = bufferedReader.readLine();
+
+                for (String str:tempArrOfWords){
+                    strings.add(counter, str);
+                    counter++;
                 }
             }
-            /*for (int i = 0; i < words.size(); i++) {
-                hashMap.put(words.get(i), 0);
+
+            HashMap<String, Integer> hashMap = new HashMap<>();
+            HashMap<String, String> hashMap1 = new HashMap<>();
+
+            for (String string:strings) {
+
+                hashMap.put(string, 0);
+                hashMap1.put(string, string);
             }
-            System.out.println(hashMap);*/
-            //System.out.println(words);
-            System.out.println(Arrays.toString(inputDataArr));
-            System.out.println(inputData);
+
+            for (String str:strings) {
+
+                if (hashMap.containsKey(str)){
+                    hashMap.replace(str, hashMap.get(str), hashMap.get(str)+1);
+                }
+            }
+
+            strings.clear();
+            strings.addAll(hashMap1.values());
+            int[]  arrOfRepetition = new int[hashMap.size()];
+            int count = 0;
+            int max = 0;
+
+            for (String str:strings) {
+
+                arrOfRepetition[count] = hashMap.get(str);
+                count++;
+            }
+
+            Arrays.sort(arrOfRepetition);
+
+            for (int i = arrOfRepetition.length-1; i >= 0; i--) {
+
+                for (String str:strings){
+
+                    if (arrOfRepetition[i] == hashMap.get(str)){
+                        System.out.println(str + "  " + arrOfRepetition[i]);
+                    }
+                }
+            }
+
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
